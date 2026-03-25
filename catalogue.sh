abf7823cbbcdf9ed7ce66483d3bcc9f9c343096b
daddanalasairam <sairamdaddanala11@gmail.com>
2026-03-25 23:32:15 +0530

    "Mongodb ip commit"

--- catalogue.sh
+++ catalogue.sh
@@ -3,13 +3,11 @@ cp catalogue.service /etc/systemd/system/catalogue.service
 
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
-
 dnf install nodejs -y
 
 useradd roboshop
 
 mkdir /app
-
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
 unzip /tmp/catalogue.zip
@@ -18,7 +16,7 @@ cd /app
 npm install
 
 dnf install mongodb-mongosh -y
-mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
+mongosh --host mongodb.sairamdevops.online </app/db/master-data.js
 
 systemctl daemon-reload
 systemctl enable catalogue
