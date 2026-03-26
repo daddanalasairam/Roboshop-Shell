4703b72c6cf561b9ca8aaf82b6e279ce6031161d
daddanalasairam <sairamdaddanala11@gmail.com>
2026-03-26 15:16:01 +0530

    color commit

--- payment.sh
+++ payment.sh
@@ -1,19 +1,29 @@
+color="\e[35m"
+no_color="\e[0m"
+echo -e "$color Copy Payment Service File $no_color"
 cp payment.service /etc/systemd/system/payment.service
 
+echo -e "$color Install Python $no_color"
 dnf install python3 gcc python3-devel -y
 
+echo -e "$color Adding Application User $no_color"
 useradd roboshop
 
+echo -e "$color Creating Application Directory $no_color"
+rm -rf /app
 mkdir /app
 
+echo -e "$color Downloading Application Content $no_color"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
 cd /app
+
+echo -e "$color Extract Application Content $no_color"
 unzip /tmp/payment.zip
 
-cd /app
+echo -e "$color Download Application Dependencies $no_color"
 pip3 install -r requirements.txt
 
+echo -e "$color Start Application Service $no_color"
 systemctl daemon-reload
-
 systemctl enable payment
 systemctl start payment
