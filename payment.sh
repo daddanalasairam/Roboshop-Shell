48a4193c1381588391c6b42baa6c5e0f84208f39
daddanalasairam <sairamdaddanala11@gmail.com>
2026-03-26 16:55:05 +0530

    functions

--- payment.sh
+++ payment.sh
@@ -1,18 +1,18 @@
 source common.sh
 app_name=payment
 
-echo -e "$color Copy Payment Service File $no_color"
+print_heading "Copy Payment Service File"
 cp payment.service /etc/systemd/system/payment.service &>>$log_file
 
-echo -e "$color Install Python $no_color"
+print_heading "Install Python"
 dnf install python3 gcc python3-devel -y &>>$log_file
 
 app_prerequisites
 
-echo -e "$color Download Application Dependencies $no_color"
+print_heading "Download Application Dependencies"
 pip3 install -r requirements.txt &>>$log_file
 
-echo -e "$color Start Application Service $no_color"
+print_heading "Start Application Service"
 systemctl daemon-reload &>>$log_file
 systemctl enable payment &>>$log_file
 systemctl start payment &>>$log_file
